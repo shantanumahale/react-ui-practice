3856d684821de4eb69b106c0a14e6ed696fb421b
Shantanu Mahale <mahaleshantanu4@gmail.com>
2026-09-02 10:47:27 +0530

    removing unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,3 @@
-import { Route, Routes } from 'react-router-dom';
-
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import { routes } from './routes';
-
 export default function App() {
-  return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<Home />} />
-
-        {routes.map(({ path, component: Component }) => (
-          <Route key={path} path={path} element={<Component />} />
-        ))}
-
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
-  );
+  return <h1>Hello World</h1>;
 }
--- src/store/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { useDispatch, useSelector } from 'react-redux';
-import type { AppDispatch, RootState } from './index';
-
-// Use these instead of the plain react-redux hooks so types flow through.
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<RootState>();
